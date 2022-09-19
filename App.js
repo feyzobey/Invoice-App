3942713b2bf1efe5a663768831b38a291e47485b
AATezgel <ahmet@tezgel.com>
2022-09-19 15:05:30 +0300

    ss

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'springgreen',
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'white',
   },
   text: {
     color: 'blue',
